0c47fc9e903dfdbc4ee1df191550b9e28b95fe6e
Jefffer <jefre123@hotmail.com>
2019-12-27 01:50:38 -0500

    Unit Test added. UnitMethod TestExchangeCurrency creation.

---
+++ Moneyxchange.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Moneyxchange.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Moneyxchange.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15fa0efb-be5c-4f1f-827a-4738cf658ed5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Moneyxchange.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BusinessLayer.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Moneyxchange.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestExchangeCurrency()
+        {
+            // Arrange Test
+            ConversionService conversionService = new ConversionService();
+            string from = "USD";
+            string to = "EUR";
+            decimal amount = 10;
+
+            // Act Test
+            decimal result = conversionService.GetExchange(from, to, amount);
+            decimal expected = (decimal)8.992650;
+
+            // Assert Test
+            Assert.AreEqual(result, expected);
+
+        }
+    }
+}
